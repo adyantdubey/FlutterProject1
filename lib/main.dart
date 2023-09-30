6e9316ab43d57724bb5463f51011fc2aee066899
adyantdubey <123817436+adyantdubey@users.noreply.github.com>
2023-09-30 08:15:02 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/home_page.dart';
+import 'package:flutter_demo/screens/home_page.dart';
+import 'package:flutter_demo/screens/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
