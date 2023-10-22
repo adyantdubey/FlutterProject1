3b5e952c3952fa01aec73ede8da0e8f7e661a0c9
adyantdubey <123817436+adyantdubey@users.noreply.github.com>
2023-10-22 09:36:59 +0530

    json file creation

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,32 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import "dart:convert";
 import 'package:flutter_demo/models/catalog.dart';
 import 'package:flutter_demo/widgets/drawer.dart';
 import 'package:flutter_demo/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loaddata();
+  }
+
+  loaddata() async {
+    final catalogjson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogjson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyitem = List.generate(20, (index) => CatalogModel.items[0]);
